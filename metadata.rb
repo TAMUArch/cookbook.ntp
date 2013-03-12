a3f7033e970da9406be37cb8ae3e3c2d5d7b089b
Jim Rosser <jarosser06@gmail.com>
2013-03-12 16:09:05 -0500

    initial commit

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name             'ntp'
+maintainer       'Texas A&M'
+maintainer_email 'jarosser06@arch.tamu.edu'
+license          'All rights reserved'
+description      'Installs/Configures ntp'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '13.2.2'
