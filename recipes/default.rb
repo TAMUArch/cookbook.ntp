87850e56bbc7c973079e0523f05c19f97d8ca2e9
Jim Rosser <jarosser06@gmail.com>
2013-03-25 14:02:12 -0500

    split off cron and daemon seperately

--- recipes/default.rb
+++ recipes/default.rb
@@ -7,28 +7,4 @@
 # All rights reserved - Do Not Redistribute
 #
 #
-case node["platform_family"]
-when "rhel"
-  node.set[:ntp][:service] = "ntpd"
-when "debian"
-  node.set[:ntp][:service] = "ntp"
-else
-  node.set[:ntp][:service] = "ntp"
-end
 
-package "ntp" do
-  action [:install]
-end
-
-template "/etc/ntp.conf" do
-  source "ntp.conf.erb"
-  variables( {:ntp_servers => ["ntp1.tamu.edu", "ntp2.tamu.edu", "ntp3.tamu.edu"]} )
-  owner "root"
-  group "root"
-  mode 0644
-  notifies :restart, "service[#{node[:ntp][:service]}]" 
-end
-
-service node[:ntp][:service] do
-  action [:enable,:start]
-end
