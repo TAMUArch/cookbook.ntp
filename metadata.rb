776b34ced615b54ff980595d4d37979b2fd7e219
Jim Rosser <jarosser06@gmail.com>
2013-04-02 08:26:48 -0500

    upped version

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email 'jarosser06@arch.tamu.edu'
 license          'MIT'
 description      'Installs/Configures ntp'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '13.3.0'
+version          '13.4.0'
 
 supports 'ubuntu', '>= 10.04'
