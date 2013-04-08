1965edf8440e14242c62161964f593a27a7ca561
Jim Rosser <jarosser06@gmail.com>
2013-04-08 14:01:22 -0500

    removed test spec file

--- spec/default_spec.rb
+++
@@ -1,8 +0,0 @@
-require 'chefspec'
-
-describe 'ntp::default' do
-  let (:chef_run) { ChefSpec::ChefRunner.new.converge 'ntp::daemon' }
-  it "does nothing" do
-    pending "Does nothing" 
-  end
-end
